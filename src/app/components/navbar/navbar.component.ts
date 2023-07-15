1abb0dc98e0e63207faacb5b25ae6b87c7374637
noko12312 <42224565+noko12312@users.noreply.github.com>
2023-07-14 19:53:21 -0500

    initial navbar layout created

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+    icons: string[] = ["👽", "🤠", "😎", "🤖", "👾", "👻", "🤓", "🤡"]
+    icon: string = this.icons[Math.floor(Math.random()*this.icons.length)];
 
+    showStats(): void{
+        console.log("stats");
+    }
+
+    showHelp(): void{
+        console.log("help");
+    }
+    
 }
