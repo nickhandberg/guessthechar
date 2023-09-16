a55a5ba37a201a9b6c37caced906181ed26e4713
noko12312 <42224565+noko12312@users.noreply.github.com>
2023-09-16 18:30:40 -0500

    base server added. Project restructured

---
+++ server/src/main/java/com/noko/guessthechar/GuessthecharApplication.java
@@ -0,0 +1,13 @@
+package com.noko.guessthechar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessthecharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessthecharApplication.class, args);
+	}
+
+}
