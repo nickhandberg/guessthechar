8aade158687b483502fc6226a590088ea89eb80c
noko12312 <42224565+noko12312@users.noreply.github.com>
2023-09-16 20:39:05 -0500

    Returning all data from mongoDB implemented

--- server/src/test/java/com/nok/gtc/GTCAppTests.java
+++ server/src/test/java/com/nok/gtc/GTCAppTests.java
@@ -1,10 +1,10 @@
-package com.noko.guessthechar;
+package com.nok.gtc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GuessthecharApplicationTests {
+class GTCAppTests {
 
 	@Test
 	void contextLoads() {
